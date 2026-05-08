6a59c76275e6bbc3204124a6bad6539e56d65e0b
Maksim Novitsky <Vf1sol@yandex.ru>
2026-05-08 14:04:22 +0300

    update-menu_2

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ menu.addEventListener("click", function(event){
     let body = document.querySelector("body");
     let mobileMenu = document.querySelector(".mobile-menu");
     if (!body.classList.contains("off-scroll") && !mobileMenu.classList.contains("hide")){body.classList.add("off-scroll");
-    }else{body.classList.remove("off-sccroll");}
+    }else{body.classList.remove("off-scroll");}
 });
 
 let classLink = ".main-link";
