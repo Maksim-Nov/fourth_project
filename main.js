1e9281f1ed40c466e482cccdafe55251e9c82543
Maksim Novitsky <Vf1sol@yandex.ru>
2026-05-29 16:45:50 +0300

    Progect-4

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ document.querySelector(".mobile-button").addEventListener("click", function(even
     document.querySelector(".mobile-menu").classList.toggle("hide");
 
     document.querySelectorAll(".mobile-btn").forEach(function(item){
-      item.classList.toggle('btn-hide');
+        item.classList.toggle('btn-hide');
    })
 });
 
@@ -34,13 +34,13 @@ menu.addEventListener('click', function(event){
 let classLink = '.main-link';
 window.onscroll = function() {
   let h = document.documentElement.clientHeight;
-  if (window.scrollY >= h*4 ){
+  if (window.scrollY >= h*4){
     classLink = '.comments-link';
   }
   else if (window.scrollY >= h*3){
     classLink = '.works-link';
   }
-  else if (window.scrollY >= h*2 ){
+  else if (window.scrollY >= h*2){
     classLink = '.skills-link';
   }
   else if (window.scrollY >= h){
